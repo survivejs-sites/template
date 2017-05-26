1a7fb28e7c0886f81e3016aa9ae3993a73c4f453
Juho Vepsalainen <bebraw@gmail.com>
2017-05-26 19:28:31 +0300

    fix: Clone over https

--- scripts/fetch-books.sh
+++ scripts/fetch-books.sh
@@ -7,12 +7,12 @@ mkdir -p books/webpack-book
 
 cd books/react-book
 if [ -d ".git" ]; then git pull;
-else git clone git@github.com:survivejs/react-book.git .;
+else git clone https://github.com/survivejs/webpack-book.git .;
 fi
 
 cd -
 
 cd books/webpack-book
 if [ -d ".git" ]; then git pull;
-else git clone git@github.com:survivejs/webpack-book.git .;
+else git clone https://github.com/survivejs/webpack-book.git .;
 fi
