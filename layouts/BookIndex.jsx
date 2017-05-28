a00fb6ee0c22056e3fb07948f4951f8e9742c740
Juho Vepsalainen <bebraw@gmail.com>
2017-05-28 16:25:08 +0300

    chore: Simplify book index template

--- layouts/BookIndex.jsx
+++ layouts/BookIndex.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import _ from 'lodash';
 import { Link } from 'react-router-dom';
-import Moment from '../components/Moment';
 
 const BookIndex = ({ section }) => (
   <div className="grid">
     <h1>Table of Contents</h1>
 
-    <ul className="post-list">{_.map(section.pages(), (page, i) => (
+    <ul className="post-list">{section.pages().map(({
+      url,
+      file: {
+        attributes: {
+          title,
+        },
+        preview,
+      },
+    }, i) => (
       <li key={`post-list-item-${i}`}>
-        <Link to={`${page.url}`}>
-          <h3 className="post-list__heading">
-            {page.file.attributes.title}
-          </h3>
+        <Link to={`${url}`}>
+          <h3 className="post-list__heading">{title}</h3>
 
-          {page.file.attributes.date ?
-            <Moment className="post__moment" datetime={page.file.attributes.date} /> :
-            null
-          }
-          <p className="post-list__preview">{page.file.preview}</p>
+          <p className="post-list__preview">{preview}</p>
         </Link>
       </li>
     ))}</ul>
