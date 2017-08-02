0ce2e5d4facbf4de103efc139ee9c52c555a24ed
Juho Vepsalainen <bebraw@gmail.com>
2017-08-02 11:34:30 +0200

    chore: Simplify external url check
    Better not to have a dependency on window.

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link as RRLink } from 'react-router-dom';
-import isExternal from 'is-url-external';
 
 const Link = ({ to, children }) => (
-  isExternal(to)
+  /^https?:\/\//.test(to)
     ? <a href={to}>{children}</a>
     : <RRLink to={to}>{children}</RRLink>
 );
